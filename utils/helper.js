439eae2f044af5ba3a30aadfb1b965fda59640ee
Uzoamaka126 <uzoamakaanyanwu26@gmail.com>
2023-09-17 12:25:17 +0100

    feat: add tokens directory and post route handler

--- utils/helper.js
+++ utils/helper.js
@@ -24,7 +24,6 @@ function isBoolValid(val) {
 }
 
 function hash(value) {
-    console.log({ value });
     if (typeof value === 'string' && value.length > 0) {
         // hash with sha-256
         const hash = crypto.createHmac('sha256', config.hashingSecret).update(value).digest('hex');
@@ -43,9 +42,27 @@ function parseJSONToObject(str) {
     }
 }
 
+function generateRandomString(num = 20) {
+    if (num > 0) {
+        const possibleCharacters = 'abcdefghijklmnopqrstuvwxyz1234567890';
+        let str = '';
+
+        for (var i = 0; i < num; i++) {
+            // get a random character from the possible characters
+            const randomChar = possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+            // append the character to the final string
+            str += randomChar
+        }
+        return str;
+    } else {
+        return false
+    }
+}
+
 module.exports = {
     isStringValid,
     hash,
     parseJSONToObject,
-    isBoolValid
+    isBoolValid,
+    generateRandomString
 }
